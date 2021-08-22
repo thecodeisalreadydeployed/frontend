819e45884d9e2d6a7bedc0f2e3ae333413dba3b0
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-08-22 15:01:55 +0700

    build(dep): react-feather

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 
+import { Award } from "react-feather";
+
 const Home: NextPage = () => {
   return (
-    <div className="py-3 text-2xl font-bold text-center bg-[#f3f3f3]">
-      Hello World
+    <div className="flex justify-center items-center py-3 space-x-5 text-2xl font-bold text-center bg-[#f3f3f3]">
+      <p>Hello World</p>
+      <Award />
     </div>
   );
 };

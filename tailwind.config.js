a181d3fb180527811e179e0ca7fa6446a5c0789f
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-08-22 14:57:33 +0700

    build(tailwind): color design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const baseColors = require("./styles/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: baseColors,
   },
   variants: {
     extend: {},

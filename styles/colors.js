1c0c31824f9eb8da5b6578ec200df8d8e8164ca1
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-10 21:22:57 +0700

    chore: rename folder and variables

---
+++ styles/colors.js
@@ -0,0 +1,52 @@
+module.exports = {
+  primary: {
+    background: "#fff",
+    accent: {
+      1: "#FAFAFA",
+      2: "#EAEAEA",
+      3: "#999",
+      4: "#888",
+      5: "#666",
+      6: "#444",
+      7: "#333",
+      8: "#111",
+    },
+    DEFAULT: "#000",
+  },
+  error: {
+    lighter: "#F7D4D6",
+    light: "#FF1A1A",
+    DEFAULT: "#E00",
+    dark: "#C50000",
+  },
+  success: {
+    lighter: "#D3E5FF",
+    light: "#3291FF",
+    DEFAULT: "#0070F3",
+    dark: "#0761D1",
+  },
+  warning: {
+    lighter: "#FFEFCF",
+    light: "#F7B955",
+    DEFAULT: "#F5A623",
+    dark: "#AB570A",
+  },
+  violet: {
+    lighter: "#E3D7FC",
+    light: "#8A63D2",
+    DEFAULT: "#7928CA",
+    dark: "#4C2889",
+  },
+  cyan: {
+    lighter: "#AAFFEC",
+    light: "#79FFE1",
+    DEFAULT: "#50E3C2",
+    dark: "#29BC9B",
+  },
+  highlight: {
+    purple: "#F81CE5",
+    magenta: "#EB367F",
+    pink: "#FF0080",
+    yellow: "#FFF500",
+  },
+};
--- theme/border-radii.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  none: "0",
-  DEFAULT: "5px",
-  full: "9999px",
-};

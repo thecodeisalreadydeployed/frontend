e6cbd9d502eb984bbe9cb1d85b0aa5e679352aa8
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-12 14:41:09 +0700

    build: add @tailwindcss/line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };

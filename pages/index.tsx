884641be7265f51fa19c59d863b9cf459fe27354
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-12 14:04:14 +0700

    feat: create per page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
-import type { NextPage } from "next";
-
 import { Award } from "react-feather";
+import { Header } from "@elements";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="flex justify-center items-center py-3 space-x-5 text-2xl font-bold text-center bg-[#f3f3f3]">
       <p>Hello World</p>
       <Award />
     </div>
+  );
+};
+
+Home.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <>
+      <Header />
+      {page}
+    </>
   );
 };
 

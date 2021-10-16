99d20f0c59468289377bb2717467f55e723404ef
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-16 21:09:46 +0700

    feat: change number of project in grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 const Project = () => {
   const router = useRouter();
   return (
-    <div className="container grid grid-cols-3 gap-6 mt-6">
+    <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-6">
       <ProjectCard name="Project 1" onClick={() => router.push("/project-1")} />
       <ProjectCard name="Project 2" onClick={() => router.push("/project-2")} />
       <ProjectCard name="Project 3" onClick={() => router.push("/project-3")} />

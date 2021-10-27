fa523eee18455ef95628b682fd5629c7ddf2d9b4
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-27 23:46:06 +0700

    feat: finish Deployment List [organism] title

--- components/organisms/overview/deployment-list.tsx
+++ components/organisms/overview/deployment-list.tsx
@@ -1,5 +1,12 @@
 const DeploymentList = () => {
-  return <div></div>;
+  return (
+    <div className="container my-12 reset">
+      <h2 className="text-2xl font-semibold">Deployments</h2>
+      <p className="my-3.5 text-sm text-primary-accent-6">
+        Deployments that are currently being worked on.
+      </p>
+    </div>
+  );
 };
 
 export { DeploymentList };

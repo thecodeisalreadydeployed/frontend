93abdf97fcc2b75dd4481f929a944606faf8964b
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-10-28 14:23:47 +0700

    chore: move deployment status to atom

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./button";
 export { Input } from "./input";
 export { Card } from "./card";
 export * from "./_svg";
+export { DeploymentStatus } from "./deployment-status";
--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,3 +1,2 @@
 export { ProjectCard } from "./project-card";
 export { NavBreadCrumb } from "./nav-breadcrumb";
-export { DeploymentStatus } from "./deployment-status";

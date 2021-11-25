8774698d10245d1dad0f50739519f9a41455d0d1
Boom PT <37561295+boompikachu@users.noreply.github.com>
2021-11-25 15:08:20 +0700

    chore: page-title allow selection

--- components/atoms/page-title/index.tsx
+++ components/atoms/page-title/index.tsx
@@ -4,11 +4,7 @@ interface PageTitleProps {
 
 const PageTitle = (props: PageTitleProps) => {
   const { children } = props;
-  return (
-    <h1 className="mb-4 text-4xl font-medium cursor-default select-none reset">
-      {children}
-    </h1>
-  );
+  return <h1 className="mb-4 text-4xl font-medium reset">{children}</h1>;
 };
 
 export { PageTitle };

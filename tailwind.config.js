0b28dd8fd44f7009cbf1b680721b34bf4461b209
Boom PT <37561295+boompikachu@users.noreply.github.com>
2022-01-02 17:50:51 +0700

    chore: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const baseColors = require(`${__dirname}/styles/colors`);
-const baseBorderRadii = require(`${__dirname}/styles/border-radii`);
-
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -9,12 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    colors: baseColors,
-    borderRadius: baseBorderRadii,
     container: {
       center: true,
       padding: "1.5rem",
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
 };

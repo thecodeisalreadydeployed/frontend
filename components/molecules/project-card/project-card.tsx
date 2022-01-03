7016ca746694067f7ba4acc7eb351afd2ec4fc56
Boom PT <37561295+boompikachu@users.noreply.github.com>
2022-01-04 00:51:04 +0700

    feat: create ProjectCard stories

--- components/molecules/project-card/project-card.tsx
+++ components/molecules/project-card/project-card.tsx
@@ -1,7 +1,6 @@
 import { Card } from "@atoms";
 interface ProjectCardProps {
   name: string;
-  description?: string;
   updatedAt?: string;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
 }
@@ -9,12 +8,14 @@ interface ProjectCardProps {
 export const ProjectCard = (props: ProjectCardProps): JSX.Element => {
   const { name, updatedAt = "N/A", onClick } = props;
 
+  const hash = name.replaceAll(" ", "");
+
   return (
     <Card onClick={onClick}>
       <div
         className="relative -mx-6 -mt-6 mb-6 h-8 bg-repeat-x opacity-60"
         style={{
-          backgroundImage: `url(https://avatars.dicebear.com/api/identicon/${name}.svg)`,
+          backgroundImage: `url(https://avatars.dicebear.com/api/identicon/${hash}.svg)`,
         }}
       />
       {/* Hard code h-12 (48px) due to when line clamped, it is changed from 48px to 46px */}

7e7d326bc6b18e5ffa76377da5206c8abee9d3be
Boom PT <37561295+boompikachu@users.noreply.github.com>
2022-01-04 12:19:48 +0700

    feat: remove pages from storybook

--- stories/pages/application.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Page from "../../pages/[project]/index";
-
-export default {
-  title: "Pages/Application",
-  component: Page,
-};
-
-export const Application = (): JSX.Element => <Page />;
--- stories/pages/project.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Page from "../../pages/index";
-
-export default {
-  title: "Pages/Project",
-  component: Page,
-};
-
-export const Project = (): JSX.Element => <Page />;

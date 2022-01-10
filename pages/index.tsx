adb61c2acd47720141f29121b9d7520e7029657c
Boom PT <37561295+boompikachu@users.noreply.github.com>
2022-01-10 14:44:19 +0700

    feat: add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,21 +43,21 @@ const Project = (): JSX.Element => {
       <PageTitle>Projects</PageTitle>
       <div className="flex mb-6 space-x-4">
         <Input
+          placeholder="Search..."
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
-          placeholder="Search..."
         />
         <Button onClick={handleOpenProjectModal}>New Project</Button>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {modifiedProjects?.map((project, index) => (
           <ProjectCard
-            projectId={project.id}
+            key={index}
             name={project.name}
+            projectId={project.id}
             updatedAt={`${formatDistanceToNowStrict(
               new Date(project.updatedAt)
             )} ago`}
-            key={index}
             onClick={() => router.push(`/${project.id}`)}
           />
         ))}

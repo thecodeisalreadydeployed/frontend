08ecc3f1232da2ba6382dfdfc90a3e542c357de1
Boom PT <37561295+boompikachu@users.noreply.github.com>
2022-01-10 13:19:11 +0700

    chore: refactor project page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useRouter } from "next/router";
 
 import { formatDistanceToNowStrict } from "date-fns";
@@ -24,6 +24,20 @@ const Project = (): JSX.Element => {
     setShowCreateProjectModal(true);
   };
 
+  const modifiedProjects = useMemo(
+    () =>
+      projects
+        ?.filter((project) =>
+          project.name.toLocaleLowerCase().includes(searchInput)
+        )
+        ?.sort((a, b) => {
+          return (
+            new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+          );
+        }),
+    [projects, searchInput]
+  );
+
   return (
     <div className="container mt-6">
       <PageTitle>Projects</PageTitle>
@@ -36,26 +50,17 @@ const Project = (): JSX.Element => {
         <Button onClick={handleOpenProjectModal}>New Project</Button>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {projects
-          ?.filter((project) =>
-            project.name.toLocaleLowerCase().includes(searchInput)
-          )
-          .sort((a, b) => {
-            return (
-              new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime()
-            );
-          })
-          .map((project, index) => (
-            <ProjectCard
-              projectId={project.id}
-              name={project.name}
-              updatedAt={`${formatDistanceToNowStrict(
-                new Date(project.updatedAt)
-              )} ago`}
-              key={index}
-              onClick={() => router.push(`/${project.id}`)}
-            />
-          ))}
+        {modifiedProjects?.map((project, index) => (
+          <ProjectCard
+            projectId={project.id}
+            name={project.name}
+            updatedAt={`${formatDistanceToNowStrict(
+              new Date(project.updatedAt)
+            )} ago`}
+            key={index}
+            onClick={() => router.push(`/${project.id}`)}
+          />
+        ))}
       </div>
       <CreateProjectModal
         showModal={showCreateProjectModal}

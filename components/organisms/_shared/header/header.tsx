eddc6fee706af7a3221e1c6ff0d90aee5f247bef
Patthanat Thanintantrakun <37561295+boompikachu@users.noreply.github.com>
2022-01-25 22:34:18 +0700

    feat: change header bg color

--- components/organisms/_shared/header/header.tsx
+++ components/organisms/_shared/header/header.tsx
@@ -8,7 +8,7 @@ import { NavBreadcrumb } from "@molecules";
 export const Header = (): JSX.Element => {
   const router = useRouter();
 
-  const { project: projectId, application: applicationId } = router.query;
+  const { application: applicationId, project: projectId } = router.query;
 
   const { data: project } = useSWR(
     projectId ? `http://localhost:3001/projects/${projectId}` : null
@@ -19,24 +19,24 @@ export const Header = (): JSX.Element => {
   );
 
   return (
-    <div className="bg-zinc-800 border-b border-b-zinc-600">
+    <div className="bg-zinc-900/50 border-b border-b-zinc-600">
       <nav className="container flex justify-between items-center h-16 sm:h-20">
         <NavBreadcrumb
-          projectName={project?.name}
           applicationName={application?.name}
-          onClickLogo={() => router.push("/")}
-          onClickProject={() => router.push(`/${projectId}`)}
+          projectName={project?.name}
           onClickApplication={() =>
             router.push(`/${projectId}/${applicationId}`)
           }
+          onClickLogo={() => router.push("/")}
+          onClickProject={() => router.push(`/${projectId}`)}
         />
         <GithubLogo
-          width="w-8"
           height="h-8"
+          width="w-8"
+          wrapperOverride="cursor-pointer"
           onClick={() =>
             router.push("https://github.com/thecodeisalreadydeployed")
           }
-          wrapperOverride="cursor-pointer"
         />
       </nav>
     </div>

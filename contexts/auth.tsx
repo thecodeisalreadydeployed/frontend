63ee0e99db199396a929d0ab18d351ddb67e28e4
Patthanat Thanintantrakun <37561295+boompikachu@users.noreply.github.com>
2022-02-13 16:55:26 +0700

    feat: add next auth

---
+++ contexts/auth.tsx
@@ -0,0 +1,18 @@
+import { useSession } from "next-auth/react";
+
+interface AuthProps {
+  children: JSX.Element;
+}
+
+export const Auth = (props: AuthProps): JSX.Element => {
+  const { children } = props;
+
+  const { data: session } = useSession({ required: true });
+  const isUser = !!session?.user;
+
+  if (isUser) {
+    return children;
+  }
+
+  return <div>Loading...</div>;
+};
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,47 @@
+import NextAuth, { User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+};
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        return signInWithEmailAndPassword(
+          auth,
+          credentials?.email ?? "",
+          credentials?.password ?? ""
+        )
+          .then((userCredential): User => {
+            return {
+              id: userCredential.user.uid,
+              name: userCredential.user.displayName,
+              email: userCredential.user.email,
+            };
+          })
+          .catch(() => {
+            return null;
+          });
+      },
+    }),
+  ],
+});

2eb758dc128af173d5a80729f292bfdf3b8f1355
Patthanat Thanintantrakun <37561295+boompikachu@users.noreply.github.com>
2022-02-16 14:35:08 +0700

    feat: change example env

--- contexts/session.tsx
+++ contexts/session.tsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
 import {
-  fetchSignInMethodsForEmail,
   getRedirectResult,
   GoogleAuthProvider,
   onAuthStateChanged,

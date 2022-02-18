399b51510ce6b2831a87ed884f028726cbaa2244
Patthanat Thanintantrakun <37561295+boompikachu@users.noreply.github.com>
2022-02-18 15:57:44 +0700

    fix: bearer token for delete method

--- services/use-delete-application.ts
+++ services/use-delete-application.ts
@@ -1,20 +1,25 @@
+import { useSession } from "contexts";
 import { useSWRConfig } from "swr";
 
 export const useDeleteApplication = (): {
   deleteApplication: typeof deleteApplication;
 } => {
   const { mutate } = useSWRConfig();
+  const { user } = useSession();
 
   const deleteApplication = async (
     applicationId: string,
     projectId: string | undefined
   ) => {
+    const idToken = await user?.getIdToken();
+
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_HOST}/apps/${applicationId}`,
       {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
+          Authorization: "Bearer " + idToken,
         },
       }
     );
--- services/use-delete-project.ts
+++ services/use-delete-project.ts
@@ -1,17 +1,22 @@
+import { useSession } from "contexts";
 import { useSWRConfig } from "swr";
 
 export const useDeleteProject = (): {
   deleteProject: typeof deleteProject;
 } => {
   const { mutate } = useSWRConfig();
+  const { user } = useSession();
 
   const deleteProject = async (projectId: string) => {
+    const idToken = await user?.getIdToken();
+
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_HOST}/projects/${projectId}`,
       {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
+          Authorization: "Bearer " + idToken,
         },
       }
     );

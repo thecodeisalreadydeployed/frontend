0a172180f4a6bbf41a11323cc17e7a8f71c74dd2
Patthanat Thanintantrakun <37561295+boomikuma@users.noreply.github.com>
2022-03-04 15:05:59 +0700

    feat: remove darkmode storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "storybook-dark-mode",
     "storybook-addon-next",
   ],
   framework: "@storybook/react",
